ed0ead9e0e0ebca8fda234d63ff29b8da453e8a5
Olashubomi <subomifasakin@outlook.com>
2024-04-12 03:35:56 +0100

    implemented modal for updating and deleting course

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -2,24 +2,34 @@ import { useLoaderData, useNavigate } from "react-router";
 
 import { FaEye } from "react-icons/fa";
 
-import { GetStudentsData } from "../Actions/SupabaseActions";
+import { FindCourse, GetStudentsData } from "../Actions/SupabaseActions";
 
 import { Navbar } from "../Components/Navbar";
 
 import { SortArrayBasedOnLetters } from "../Actions/HelperActions";
+import { IoIosAdd, IoIosAddCircle, IoIosCloseCircle } from "react-icons/io";
+import { Box, Modal, TextField, Typography } from "@mui/material";
+import { useEffect, useReducer, useRef, useState } from "react";
+import { Button } from "../Components/Button";
 
 function HomePage() {
+  const [modalOpen, setModalOpen] = useState(false);
+
   const { courses, matric_no, college, dept } = useLoaderData();
 
   const navigate = useNavigate();
 
   const courses2 = SortArrayBasedOnLetters(JSON.parse(courses));
 
+  function handleToggleModal() {
+    setModalOpen((c) => !c);
+  }
+
   return (
     <>
       <Navbar />
 
-      <main className="row-span-2 flex flex-col space-y-4  bg-primaryBgColor  px-6 py-4 lg:py-2 ">
+      <main className="relative row-span-2 flex flex-col space-y-4  bg-primaryBgColor  px-6 py-4 lg:py-2 ">
         <div className="flex cursor-default flex-col space-y-2  text-xs text-white lg:flex-row lg:items-center lg:justify-between">
           <p className=" rounded-sm bg-bellsBlue px-2 py-2 shadow-sm">
             Matric No: <span className="font-semibold">{matric_no}</span>
@@ -34,7 +44,7 @@ function HomePage() {
           </p>
         </div>
 
-        <div className="w-full overflow-y-auto">
+        <div className="relative w-full overflow-y-auto">
           <h2 className="cursor-default border border-stone-400 bg-bellsBlue p-2 text-xs font-semibold uppercase text-white lg:text-left">
             Courses Offered
           </h2>
@@ -73,11 +83,225 @@ function HomePage() {
               })}
             </tbody>
           </table>
+
+          {modalOpen ? (
+            <ModalComp
+              handleToggleModal={handleToggleModal}
+              selectedCourses={courses2}
+            />
+          ) : null}
         </div>
+        <ModalButton onClick={handleToggleModal} />
       </main>
     </>
   );
 }
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 600,
+  bgcolor: "#F7F4EA",
+  border: "1px solid #000",
+  boxShadow: 24,
+  borderRadius: "5px",
+  p: 4,
+  fontFamily: "Poppins, sans-serif",
+};
+
+function ModalComp({ handleToggleModal, selectedCourses }) {
+  const [updatedCourses, setUpdatedCourses] = useState(selectedCourses);
+
+  const [{ foundCourses, loading, error, searchParam }, dispatch] = useReducer(
+    Step3Reducer,
+    initialStep3State,
+  );
+
+  const searchRef = useRef(null);
+
+  function addCourse(e, course) {
+    e.stopPropagation();
+    e.preventDefault();
+    if (selectedCourses.includes(course)) return;
+
+    setUpdatedCourses((c) => [...c, course]);
+
+    dispatch({ label: "clearSearchParam" });
+    searchRef.current.focus();
+  }
+
+  function removeCourse(e, course) {
+    e.stopPropagation();
+    e.preventDefault();
+
+    setUpdatedCourses((c) => c.filter((c2) => c2 !== course));
+  }
+
+  useEffect(
+    function () {
+      const abortController = new AbortController();
+
+      async function findCourse() {
+        try {
+          const courses = await FindCourse(searchParam, abortController);
+
+          dispatch({ label: "foundCourses", payload: courses });
+        } catch (err) {
+          if (abortController.signal.aborted) {
+            dispatch({ label: "stopLoading" });
+            return;
+          }
+
+          dispatch({ label: "error", payload: err.message });
+        }
+      }
+
+      if (searchParam.length > 1) {
+        findCourse();
+      }
+
+      return () => abortController.abort();
+    },
+    [searchParam],
+  );
+
+  return (
+    <Modal open={true} onClose={handleToggleModal}>
+      <div className="absolute left-1/2 top-1/2 w-3/4 -translate-x-1/2 -translate-y-1/2 rounded-sm border border-stone-500 bg-primaryBgColor px-4 py-6 ">
+        <form className="space-y-3">
+          <Typography
+            sx={{
+              fontSize: "1.05rem",
+              fontWeight: 600,
+              textAlign: "left",
+              textTransform: "capitalize",
+              fontFamily: "Poppins,sans-serif",
+            }}
+          >
+            Edit Courses
+          </Typography>
+
+          <TextField
+            id="outlined-basic"
+            label="Course Code"
+            variant="outlined"
+            fullWidth
+            ref={searchRef}
+            onChange={(e) =>
+              dispatch({ label: "searching", payload: e.target.value })
+            }
+          />
+
+          <ul className="max-h-64 overflow-y-auto p-2 lg:max-h-64">
+            {searchParam && !loading ? (
+              <>
+                <h2 className="text-xs">Found Courses</h2>
+                {foundCourses.length ? (
+                  foundCourses.map((course, i) => {
+                    return !selectedCourses.includes(course.course_code) ? (
+                      <div
+                        className="flex items-center justify-between py-2"
+                        key={i}
+                      >
+                        <span> {course.course_code}</span>
+
+                        <div className="flex items-center">
+                          <button
+                            className="text-sm transition-colors duration-300 ease-in-out hover:text-hoverBellsBlue active:text-hoverBellsBlue"
+                            onClick={(e) => addCourse(e, course.course_code)}
+                          >
+                            <IoIosAddCircle className=" text-xl text-black hover:text-hoverBellsBlue" />
+                          </button>
+                        </div>
+                      </div>
+                    ) : null;
+                  })
+                ) : (
+                  <p className="text-center text-sm">No course found</p>
+                )}
+              </>
+            ) : null}
+
+            {/** if we are still searching */}
+            {loading && searchParam ? (
+              <p className="text-center text-sm">Finding Course...</p>
+            ) : null}
+
+            {/*if we are not searching for anything */}
+            {!searchParam ? (
+              <>
+                <h2 className="text-xs">
+                  Selected Courses ({updatedCourses.length})
+                </h2>
+
+                {updatedCourses.map((course, i) => {
+                  return (
+                    <div
+                      className="flex items-center justify-between py-2"
+                      key={i}
+                    >
+                      <span> {course}</span>
+
+                      <div className="flex items-center px-1">
+                        <button
+                          className="text-sm transition-colors duration-300 ease-in-out hover:text-hoverBellsBlue active:text-hoverBellsBlue"
+                          onClick={(e) => removeCourse(e, course)}
+                        >
+                          <IoIosCloseCircle className=" text-2xl text-black hover:text-hoverBellsBlue lg:text-xl" />
+                        </button>
+                      </div>
+                    </div>
+                  );
+                })}
+              </>
+            ) : null}
+          </ul>
+
+          <Button label={"Update Courses"} type="full" />
+        </form>
+      </div>
+    </Modal>
+  );
+}
+
+function ModalButton({ onClick }) {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className="absolute bottom-12 right-12 rounded-full bg-bellsBlue bg-opacity-50 p-2 text-center text-3xl text-white transition-colors duration-300 ease-in-out hover:bg-opacity-100 active:bg-opacity-100"
+    >
+      <IoIosAdd />
+    </button>
+  );
+}
+
+function Step3Reducer(state, { payload, label }) {
+  switch (label) {
+    case "searching":
+      return { ...state, searchParam: payload, loading: true };
+
+    case "stopLoading":
+      return { ...state, loading: false };
+
+    case "error":
+      return { ...state, loading: false, error: payload };
+
+    case "foundCourses":
+      return { ...state, loading: false, foundCourses: payload };
+
+    case "clearSearchParam":
+      return { ...state, searchParam: "" };
+  }
+}
+
+const initialStep3State = {
+  searchParam: "",
+  foundCourses: [],
+  error: "",
+  loading: false,
+};
 
 export async function HomeLoader() {
   const studData = await GetStudentsData();
